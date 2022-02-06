3d34199bea54902aba1629979a2b15bb3ae88afa
sam-mcmurray <smcmurray1126@gmail.com>
2022-02-06 21:52:24 +0100

    navigation with react router

--- breakout/src/App.js
+++ breakout/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Body from "./components/Body";
+import Welcome from "./view/Welcome";
+import Leaderboard from "./view/Leaderboard";
+import Breakout from "./view/Breakout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Body />}>
+        <Route path="/" exact element={<Welcome />} />
+        <Route path="/breakout" element={<Breakout />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ breakout/src/views/Breakout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Breakout() {
+  return (
+    <div>
+      <h1>Breakout Page</h1>
+    </div>
+  );
+}
+
+export default Breakout;
