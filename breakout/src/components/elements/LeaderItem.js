9720e253ebb1e8269c0e08d22e8a57cae32c9522
sam-mcmurray <smcmurray1126@gmail.com>
2022-03-27 21:54:49 +0200

    leaderboard name added

--- breakout/src/components/elements/LeaderItem.js
+++ breakout/src/components/elements/LeaderItem.js
@@ -7,7 +7,7 @@ function LeaderItem(props) {
   return (
     <li>
       <Card className="leader-item__content">
-        <h1 className="h1">{props.username}</h1>
+        <h1 className="h1">{props.name}</h1>
         <h2>TOP LEVEL: {props.level}</h2>
         <h3>TOP SCORE: {props.score}</h3>
       </Card>
